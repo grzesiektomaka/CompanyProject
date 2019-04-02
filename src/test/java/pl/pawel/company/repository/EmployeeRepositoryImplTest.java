ab1218ce54fe97bee9ba89c66c478fee0192a134
Grzegorz <grzesiektomaka@gmail.com>
2019-04-02 15:48:17 +0200

    The basis of the program

---
+++ src/test/java/pl/pawel/company/repository/EmployeeRepositoryImplTest.java
@@ -0,0 +1,49 @@
+package pl.pawel.company.repository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import pl.pawel.company.entity.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+import static org.junit.Assert.*;
+
+public class EmployeeRepositoryImplTest {
+
+
+    private  Employee employee ;
+    private List<Employee> employeeList = new ArrayList<>();
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void createTest() {
+    }
+
+    @Test
+    public void updateTest() {
+
+    }
+
+    @Test
+    public void getTest() {
+
+    }
+
+    @Test
+    public void deleteTest() {
+
+    }
+}
